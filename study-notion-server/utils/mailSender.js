ee55a0259b113db139a9e39076371b436510d354
Bhupendra Singh <bhupendra.singh.vector@gmail.com>
2026-02-14 22:06:29 +0530

    fix nodemailer config for render

--- study-notion-server/utils/mailSender.js
+++ study-notion-server/utils/mailSender.js
@@ -3,13 +3,18 @@ const nodemailer = require("nodemailer")
 const mailSender = async (email, title, body) => {
   try {
     let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: process.env.MAIL_PORT,
+      host: "smtp.gmail.com",
+      port: 587,
       secure: false,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
+      requireTLS: true,
+      tls: {
+        rejectUnauthorized: false,
+      },
+      connectionTimeout: 20000,
     })
 
     let info = await transporter.sendMail({
