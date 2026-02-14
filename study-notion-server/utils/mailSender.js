6a06914a1598f6ec590373aede3ae0b789017a7a
Bhupendra Singh <bhupendra.singh.vector@gmail.com>
2026-02-14 21:47:12 +0530

    fix nodemailer config for render

--- study-notion-server/utils/mailSender.js
+++ study-notion-server/utils/mailSender.js
@@ -1,27 +1,30 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer")
 
 const mailSender = async (email, title, body) => {
   try {
     let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
-      secure: false,
-    });
+    })
 
     let info = await transporter.sendMail({
-      from: `"Studynotion " <${process.env.MAIL_USER}>`, // sender address
-      to: `${email}`, // list of receivers
-      subject: `${title}`, // Subject line
-      html: `${body}`, // html body
-    })(info.response);
-    return info;
-  } catch (error) {
-    error.message;
-    return error.message;
-  }
-};
+      from: `"Studynotion" <${process.env.MAIL_USER}>`,
+      to: email,
+      subject: title,
+      html: body,
+    })
 
-module.exports = mailSender;
+    console.log("Email sent:", info.response)
+    return info
+  } catch (error) {
+    console.log("Mail Error:", error)
+    return error.message
+  }
+}
+
+module.exports = mailSender
