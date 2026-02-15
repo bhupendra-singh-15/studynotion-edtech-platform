718013349b0338f8a8bd34b1b94272b1bf7eaaed
Bhupendra Singh <bhupendra.singh.vector@gmail.com>
2026-02-15 10:16:16 +0530

    Completed full-stack StudyNotion edtech platform

--- study-notion-server/utils/mailSender.js
+++ study-notion-server/utils/mailSender.js
@@ -1,45 +1,25 @@
 const nodemailer = require("nodemailer")
-const { google } = require("googleapis")
-
-const OAuth2 = google.auth.OAuth2
 
 const mailSender = async (email, title, body) => {
   try {
-    const oauth2Client = new OAuth2(
-      process.env.GOOGLE_CLIENT_ID,
-      process.env.GOOGLE_CLIENT_SECRET,
-      "https://developers.google.com/oauthplayground"
-    )
-
-    oauth2Client.setCredentials({
-      refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
-    })
-
-    const accessToken = await oauth2Client.getAccessToken()
-
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
       auth: {
-        type: "OAuth2",
         user: process.env.MAIL_USER,
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-        accessToken: accessToken.token,
+        pass: process.env.MAIL_PASS,
       },
+      secure: false,
     })
 
-    const info = await transporter.sendMail({
-      from: `"Studynotion" <${process.env.MAIL_USER}>`,
-      to: email,
-      subject: title,
-      html: body,
-    })
-
-    console.log("Email sent:", info.response)
+    let info = await transporter.sendMail({
+      from: `"Studynotion " <${process.env.MAIL_USER}>`, // sender address
+      to: `${email}`, // list of receivers
+      subject: `${title}`, // Subject line
+      html: `${body}`, // html body
+    })(info.response)
     return info
   } catch (error) {
-    console.log("Mail Error:", error)
+    error.message
     return error.message
   }
 }
